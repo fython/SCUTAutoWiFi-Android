f2121b5013ce8093d01b4c85123ca2497a67692c
Fung <fython@163.com>
2017-03-31 19:42:16 +0800

    MainActivity: Monitor network changes & Account card
    Signed-off-by: Fung <fython@163.com>

--- app/src/main/kotlin/moe/feng/scut/autowifi/MainActivity.kt
+++ app/src/main/kotlin/moe/feng/scut/autowifi/MainActivity.kt
@@ -1,7 +1,14 @@
 package moe.feng.scut.autowifi
 
 import android.app.Activity
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.net.ConnectivityManager
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
 
 import moe.feng.material.statusbar.StatusBarCompat
 import moe.feng.scut.autowifi.api.DormitoryApi
@@ -12,6 +19,9 @@ import org.jetbrains.anko.*
 class MainActivity : Activity(), AnkoLogger {
 
 	val fab by lazy { find<FloatingActionButton>(R.id.fab) }
+	val statusText by lazy { find<TextView>(R.id.status_text) }
+	val userName by lazy { find<EditText>(R.id.user_name) }
+	val userPwd by lazy { find<EditText>(R.id.user_pwd) }
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		StatusBarCompat.setUpActivity(this)
@@ -37,13 +47,32 @@ class MainActivity : Activity(), AnkoLogger {
 			}
 			doConnect()
 		}
+
+		/** Check manually after launching */
+		networkStateReceiver.checkManually(this)
+	}
+
+	override fun onResume() {
+		val filter = IntentFilter()
+		filter.addAction(ConnectivityManager.CONNECTIVITY_ACTION)
+		registerReceiver(networkStateReceiver, filter)
+		super.onResume()
+	}
+
+	override fun onPause() {
+		unregisterReceiver(networkStateReceiver)
+		super.onPause()
+	}
+
+	private fun setStatusText(any : Any) {
+		statusText.text = getString(R.string.status_text_format, any)
 	}
 
 	private fun doConnect() {
 		doAsync {
 			val result = DormitoryApi
 					.setCurrentIp(WifiUtils.getCurrentIP(this@MainActivity))
-					.connect(username = "", password = "")
+					.connect(username = userName.text.toString(), password = userPwd.text.toString())
 			val errCode = DormitoryApi.checkError()
 			uiThread {
 				val dialog = AlertDialogBuilder(this@MainActivity)
@@ -54,5 +83,46 @@ class MainActivity : Activity(), AnkoLogger {
 			}
 		}
 	}
+
+	private class NetworkStateReceiver(
+			val disconnected: () -> Unit, val connectedMobile: () -> Unit, val connectedWifi: (Boolean) -> Unit
+	) : BroadcastReceiver() {
+
+		override fun onReceive(context: Context?, intent: Intent?) {
+			context?.let { checkManually(it) }
+		}
+
+		fun checkManually(context : Context) {
+			val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+			val wifiNetworkInfo = cm.getNetworkInfo(ConnectivityManager.TYPE_WIFI)
+			val dataNetworkInfo = cm.getNetworkInfo(ConnectivityManager.TYPE_MOBILE)
+
+			if (!dataNetworkInfo.isConnected && !wifiNetworkInfo.isConnected) {
+				disconnected.invoke()
+			} else if (dataNetworkInfo.isConnected && !wifiNetworkInfo.isConnected) {
+				connectedMobile.invoke()
+			} else if (!dataNetworkInfo.isConnected && wifiNetworkInfo.isConnected) {
+				connectedWifi.invoke(WifiUtils.isSCUTSSID(context))
+			}
+		}
+
+	}
+
+	private val networkStateReceiver = NetworkStateReceiver(
+			disconnected = {
+				setStatusText(getString(R.string.status_text_disconnected))
+			},
+			connectedMobile = {
+				setStatusText(getString(R.string.status_text_mobile_connected))
+			},
+			connectedWifi = {
+				if (it) {
+					setStatusText(getString(R.string.status_text_wifi_connected_scut))
+				} else {
+					setStatusText(getString(R.string.status_text_wifi_connected_other))
+				}
+			}
+	)
 
 }
